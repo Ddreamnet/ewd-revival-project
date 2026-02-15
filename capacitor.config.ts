238adc193b40b4e595cb49b3057a93369da35aef
Fatih Boke <fatihbkefatih@gmail.com>
2026-02-15 19:10:42 +0300

    WIP: add android project/resources + config updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.englishwithdilara.app',
   appName: 'English with Dilara',
   webDir: 'dist',
-  server: {
-    url: 'https://62e18515-e696-45ea-b179-36f540b9f09d.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
 };
 
 export default config;
